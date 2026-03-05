608922802bc0513bef3a8166b25b145d9b108453
suikapon <reikotracker@gmail.com>
2026-03-05 18:38:38 +0100

    modMain

--- core/src/main/java/io/github/dodgedn/Main.java
+++ core/src/main/java/io/github/dodgedn/Main.java
@@ -1,32 +1,68 @@
 package io.github.dodgedn;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class Main implements ApplicationListener {
+    Texture bgTexture;
+    Texture bartTexture;
+    Texture blueBartTexture;
+    Texture bartHurtedTexture;
+    Texture bulletTexture;
+    Texture duffTexture;
+    Texture homerTexture;
+    Texture homerSemiSleepingTexture;
+    Texture homerSleepingTexture;
+
+    Sound bDoh;
+    Sound hDoh;
+    Music music;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        bgTexture = new Texture("bg2.png");
+        bartTexture = new Texture("bartolo.png");
+        blueBartTexture = new Texture("bartoloblue.png");
+        bartHurtedTexture = new Texture("bartolohurt.png");
+        bulletTexture = new Texture("bullet.png");
+        duffTexture = new Texture("duff.png");
+        homerTexture = new Texture("homer.png");
+        homerSemiSleepingTexture = new Texture("homeralmsleep.png");
+        homerSleepingTexture = new Texture("homersleep.png");
+        bDoh = Gdx.audio.newSound(Gdx.files.internal("bart_doh.mp3"));
+        hDoh = Gdx.audio.newSound(Gdx.files.internal("homer_doh.mp3"));
+        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+
     }
 }
